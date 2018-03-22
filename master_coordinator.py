08be177c2d462e50f63966a78f89d26b57d7a463
drsagitn <nguyenlongthinh@gmail.com>
2018-03-22 15:01:36 +0900

    Fix name bug

--- master_coordinator.py
+++ master_coordinator.py
@@ -64,7 +64,7 @@ def get_model(model_name):
     model_dir = conf['MODEL_DIR']
     if model_name == "BEST_MODEL":
         model_name = conf['BEST_MODEL']
-    model_path = os.path.join(model_dir, model_name)
+    model_path = os.path.join(model_dir, model_name + ".h5")
     return get_file(model_path)
 
 
