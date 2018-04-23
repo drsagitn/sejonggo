d574617eb3124e0f7491d0efc63bd94fd46dd84e
drsagitn <nguyenlongthinh@gmail.com>
2018-04-23 15:07:28 +0900

    main_selfplay to check not generate game data for same best model many time

--- main_selfplay.py
+++ main_selfplay.py
@@ -2,6 +2,7 @@ from predicting_queue_worker import init_predicting_workers, destroy_predicting_
 from conf import conf
 from selfplay_worker import NoModelSelfPlayWorker
 from utils import init_directories, clean_up_empty
+from predicting_queue_worker import put_name_request
 from scpy import sync_all_game_data
 import sys
 
@@ -11,9 +12,18 @@ def main():
     init_directories()
     clean_up_empty()
     GPUs = conf['GPUs']
+    finished_best_model_name = None
     while True:
-        # SELF-PLAY
         init_predicting_workers(GPUs)
+        #  Check if we did self-play on this best model or not
+        curr_best_model_name = put_name_request("BEST")
+        if curr_best_model_name != finished_best_model_name:
+            finished_best_model_name = curr_best_model_name
+        else:
+            print("No new best model for self-playing. Stopping..")
+            destroy_predicting_workers()
+            break
+
         workers = [NoModelSelfPlayWorker(i) for i in GPUs]
         for p in workers: p.start()
         for p in workers: p.join()
