5ab690426eda49efe263d93db26e74556066f41b
thinh <nguyenlongthinh@gmail.com>
2018-08-14 14:00:29 +0900

    fix bug game play

--- kgs_data_generator.py
+++ kgs_data_generator.py
@@ -79,7 +79,11 @@ class KGSDataGenerator(keras.utils.Sequence):
 
     def get_movelist(self):
         game_file = self.gamelist.pop()
-        return self.play_game_kgs(os.path.join(game_file))
+        try:
+            movelist = self.play_game_kgs(os.path.join(game_file))
+        except:
+            return self.get_movelist()
+        return movelist
 
     def get_movelist_by_batchsize(self):
         while len(self.movelist) < self.batch_size*8:
